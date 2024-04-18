96705787d686f8d23f0d0c969f2e61495c850f25
BjornRo <54636059+BjornRo@users.noreply.github.com>
2024-04-18 23:27:34 +0200

    reorganized

--- zigbee_controller/mytypes.py
+++ zigbee_controller/mytypes.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from abc import ABC, abstractmethod
+from abc import ABC
 from dataclasses import dataclass
 from enum import IntEnum, StrEnum
 from typing import Literal, TypedDict
