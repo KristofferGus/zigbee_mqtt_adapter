bde16dae1e8743d215a4663f81d9aef05ee14469
BjornRo <54636059+BjornRo@users.noreply.github.com>
2024-04-19 14:10:19 +0200

    updated descriptions

--- zigbee_controller/routing.py
+++ zigbee_controller/routing.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import orjson as json
 from const import OK
 from litestar import get, post
 from litestar.connection import ASGIConnection
@@ -28,25 +29,25 @@ class RootRouter(Controller):
     @get(
         path="/",
         description=gen_description(
-            "Returns json with Keys: 'light', 'remotes', 'states'",
-            "light/remote-value: list of valid ids",
-            "state-value: dict of index and their given name, index should be used",
+            "Returns json with Keys: **'light', 'remotes', 'states'**",
+            "\tlight/remote-value: list[ID(str)]",
+            f"\tmode-value: {dict((str(x.value), x.name) for x in Mode)} // Real result",
         ),
     )
     async def root(self) -> dict[str, list[str] | dict[int, str]]:
         return {
             "lights": controller.new_lightsID,
             "remotes": controller.new_remotesID,
-            "states": {x.value: x.name for x in Mode},
+            "mode": {x.value: x.name for x in Mode},
         }
 
-    @get(path="/on", status_code=200, description="Turns state on")
+    @get(path="/on", status_code=200, description="Turns state **on**")
     async def on(self) -> str:
         async with controller.lock:
             await controller.publish_all_lights(LampMessage(state="ON"))
         return OK
 
-    @get(path="/off", status_code=200, description="Turns state off")
+    @get(path="/off", status_code=200, description="Turns state **off**")
     async def off(self) -> str:
         async with controller.lock:
             await controller.publish_all_lights(LampMessage(state="OFF"))
@@ -54,7 +55,7 @@ class RootRouter(Controller):
 
     @get(
         path=["/reset", "/reset/{value_uint8:int}"],
-        description="Resets the light to halfish brightness, if path (0-255) is given -> reset to these",
+        description="Resets the light to halfish brightness, if path **(0-255)** is given -> reset to these",
     )
     async def default_reset(self, value_uint8: BRIGHTNESS_UNIT8 = 127) -> str:
         async with controller.lock:
@@ -71,9 +72,10 @@ class RootRouter(Controller):
     @get(
         path=["/state/{id:int}", "/state/{id:int}/{setting:int}"],
         description=gen_description(
-            "If only one path is given, then use default setting of this state.",
+            "If only one path is given, then use default setting of this mode.",
             "Second path sets the setting of the state",
-            f"Valid values of states is: {list(range(len(Mode)))}",
+            f"Mapping: **{dict((x.value, x.name) for x in Mode)}**",
+            f"\tid: {list(range(len(Mode)))}",
         ),
         raises=[ClientException],
     )
@@ -87,14 +89,15 @@ class RootRouter(Controller):
         return OK
 
     _ldescription = gen_description(
-        "ONLY Allowed during Default state; reset or change state first!",
-        f"index (0-{len(controller._lights)-1}) -> light starting from: door(0)->longwall-shortwall-windows(max)",
-        " index uses comma separated ints: light/1 | light/0,2,3 | light?index=1,2,3",
-        "No index given, then all lights",
-        "(st)ate: ON | OFF",
-        "brightness: 0-255",
-        "color: [RED_UINT8, GREEN_UINT8, BLUE_UINT8]",
-        "color_temp: 250-454 (~2500k-4540k)",
+        "### ONLY Allowed during Default state; reset or change state first!",
+        f"index **(0-{len(controller._lights)-1})** -> light starting from: "
+        f"door(**0**)->longwall-shortwall-windows(**{len(controller._lights)-1}**)",
+        " index uses comma separated ints: **light/1 | light/0,2,3 | light?index=1,2,3**",
+        "***No index given, then all lights***",
+        "\t(st)ate: ON | OFF | null",
+        "\tbrightness: 0-255",
+        "\tcolor: [RED_UINT8, GREEN_UINT8, BLUE_UINT8]",
+        "\tcolor_temp: 250-454 (~2500k-4540k)",
     )
 
     @get(
@@ -108,11 +111,19 @@ class RootRouter(Controller):
         index: str | None = None,
         st: LampState | None = None,
         brightness: BRIGHTNESS_UNIT8 | None = None,
-        color: RGB | None = None,
+        color: str | None = None,
         color_temp: COLORTEMP250_454 | None = None,
     ) -> str:
+        if color:
+            try:
+                r, g, b = json.loads(color)
+                _color = (r, g, b)
+            except:
+                raise ClientException("Too few/many colors given")
+        else:
+            _color = None
         await self._validate_lamp_message_publish(
-            index=index, state=st, brightness=brightness, color=color, color_temp=color_temp
+            index=index, state=st, brightness=brightness, color=_color, color_temp=color_temp
         )
         return OK
 
